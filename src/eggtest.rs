65c671dcd1dcba77a53f9a05ef2cc0ac12128dbc
memoryleak47 <memoryleak47@protonmail.ch>
2024-02-07 22:49:50 +0100

    add +, * and const numbers

--- src/eggtest.rs
+++ src/eggtest.rs
@@ -2,15 +2,29 @@ use egg::*;
 
 define_language! {
     enum Term {
-        "abs" = Abstraction([Id; 2]),
+        "abs" = Abstraction([Id; 2]), // TODO the left arg of `abs` should only be a variable, not a full-blown Term.
         "app" = Application([Id; 2]),
         Symb(Symbol),
+
+        "+" = Add([Id; 2]),
+        "*" = Mul([Id; 2]),
+        Num(i32),
     }
 }
 
 fn make_rules() -> Vec<Rewrite<Term, ()>> {
     vec![
         rewrite!("beta-reduction"; "(app (abs ?v ?b) ?c)" => { BetaReduction }),
+        rewrite!("mul-0"; "(* ?a 0)" => "0"),
+        rewrite!("mul-1"; "(* ?a 1)" => "?a"),
+        rewrite!("mul-comm"; "(* ?a ?b)" => "(* ?b ?a)"),
+        rewrite!("mul-assoc"; "(* ?a (* ?b ?c))" => "(* (* ?a ?b) ?c)"),
+
+        rewrite!("add-0"; "(+ ?a 0)" => "?a"),
+        rewrite!("add-comm"; "(+ ?a ?b)" => "(+ ?b ?a)"),
+        rewrite!("add-assoc"; "(+ ?a (+ ?b ?c))" => "(+ (+ ?a ?b) ?c)"),
+
+        rewrite!("distr"; "(* (+ ?a ?b) ?c)" => "(+ (* ?a ?c) (* ?b ?c))"),
     ]
 }
 
@@ -21,6 +35,14 @@ fn substitute(v: Id, b: Id, c: Id, eg: &mut EGraph<Term, ()>, touched: &mut Vec<
     // TODO this "choice" might be suboptimal!
     let l = eg[b].nodes.iter().next().unwrap().clone();
 
+    let mut map = |[l, r]: [Id; 2], op: fn([Id; 2]) -> Term, eg: &mut EGraph<Term, ()>| {
+        let l = substitute(v, l, c, eg, touched);
+        let r = substitute(v, l, c, eg, touched);
+        let ret = eg.add(op([l, r]));
+        touched.push(ret);
+        ret
+    };
+
     match l {
         Term::Abstraction([v2, y]) if eg.find(v2) == eg.find(v) => b,
         Term::Abstraction([v2, y]) => {
@@ -29,15 +51,12 @@ fn substitute(v: Id, b: Id, c: Id, eg: &mut EGraph<Term, ()>, touched: &mut Vec<
             touched.push(ret);
             ret
         }
-        Term::Application([l, r]) => {
-            let l = substitute(v, l, c, eg, touched);
-            let r = substitute(v, l, c, eg, touched);
-            let ret = eg.add(Term::Application([l, r]));
-            touched.push(ret);
-            ret
-        },
+        Term::Application(l) => map(l, Term::Application, eg),
         Term::Symb(v2) if eg.find(v) == eg.find(b) => c,
         Term::Symb(v2) => b,
+        Term::Add(l) => map(l, Term::Add, eg),
+        Term::Mul(l) => map(l, Term::Mul, eg),
+        Term::Num(_) => b,
     }
 }
 
@@ -82,4 +101,6 @@ pub fn main() {
 
     assert_eq!(simplify(&s), "(abs x x)");
     assert_eq!(simplify(&infinite_loop), infinite_loop);
+
+    assert_eq!(simplify("(+ x (* 2 0))"), "x");
 }
