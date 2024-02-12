988849cc9a96a61a2641553969510a2f2b164819
memoryleak47 <memoryleak47@protonmail.ch>
2024-02-12 11:15:09 +0100

    remove redundant language impl

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,32 @@
-#![feature(box_patterns)]
+mod term;
+use term::*;
 
-mod lang;
-pub use lang::*;
-
-mod tokenize;
-pub use tokenize::*;
-
-mod parser;
-pub use parser::*;
-
-mod run;
-pub use run::*;
-
-mod egg_compat;
-pub use egg_compat::*;
-
-mod eggtest;
-
-use std::fs::*;
-use std::io::Read;
+use egg::*;
 
 fn main() {
-    eggtest::main();
+    assert_eq!(simplify("(app (lam v b) c)"), "b");
+    assert_eq!(simplify("(app (lam v v) c)"), "c");
+
+    let omega = "(lam x (app x x))";
+    let infinite_loop = format!("(app {omega} {omega})");
+
+    let id = "(lam x x)";
+    let t = "(lam x (lam y x))";
+    let s = format!("(app (app {t} {id}) {infinite_loop})");
+
+    assert_eq!(simplify(&s), "(lam x x)");
+    assert_eq!(simplify(&infinite_loop), infinite_loop);
+
+    assert_eq!(simplify("(+ x (* 2 0))"), "x");
+
 }
 
-fn main2() {
-    let filename: String = std::env::args().nth(1).unwrap_or(String::from("file.l"));
-    let mut f = File::open(filename).unwrap();
-    let mut data = String::new();
-    f.read_to_string(&mut data).unwrap();
-
-    let l = parse(data);
-    let l = run(l);
-    println!("{}", l);
+fn simplify(s: &str) -> String {
+    let expr: RecExpr<Term> = s.parse().unwrap();
+    let runner = Runner::default().with_expr(&expr).run(&make_rules());
+    let root = runner.roots[0];
+    let extractor = Extractor::new(&runner.egraph, AstSize);
+    let (best_cost, best) = extractor.find_best(root);
+    println!("Simplified {} to {} with cost {}", expr, best, best_cost);
+    best.to_string()
 }
--- src/term.rs
+++ src/term.rs
@@ -1,7 +1,7 @@
 use egg::*;
 
 define_language! {
-    enum Term {
+    pub enum Term {
         "lam" = Abstraction([Id; 2]), // TODO the left arg of `lam` should only be a variable, not a full-blown Term.
         "app" = Application([Id; 2]),
         Symb(Symbol),
@@ -12,7 +12,7 @@ define_language! {
     }
 }
 
-fn make_rules() -> Vec<Rewrite<Term, ()>> {
+pub fn make_rules() -> Vec<Rewrite<Term, ()>> {
     vec![
         rewrite!("beta-reduction"; "(app (lam ?v ?b) ?c)" => { BetaReduction }),
         rewrite!("mul-0"; "(* ?a 0)" => "0"),
@@ -37,14 +37,14 @@ fn substitute(v: Id, b: Id, c: Id, eg: &mut EGraph<Term, ()>, touched: &mut Vec<
 
     let mut map = |[l, r]: [Id; 2], op: fn([Id; 2]) -> Term, eg: &mut EGraph<Term, ()>| {
         let l = substitute(v, l, c, eg, touched);
-        let r = substitute(v, l, c, eg, touched);
+        let r = substitute(v, r, c, eg, touched);
         let ret = eg.add(op([l, r]));
         touched.push(ret);
         ret
     };
 
     match l {
-        Term::Abstraction([v2, y]) if eg.find(v2) == eg.find(v) => b,
+        Term::Abstraction([v2, _]) if eg.find(v2) == eg.find(v) => b,
         Term::Abstraction([v2, y]) => {
             let id = substitute(v, y, c, eg, touched);
             let ret = eg.add(Term::Abstraction([v2, id]));
@@ -52,8 +52,8 @@ fn substitute(v: Id, b: Id, c: Id, eg: &mut EGraph<Term, ()>, touched: &mut Vec<
             ret
         }
         Term::Application(l) => map(l, Term::Application, eg),
-        Term::Symb(v2) if eg.find(v) == eg.find(b) => c,
-        Term::Symb(v2) => b,
+        Term::Symb(_) if eg.find(v) == eg.find(b) => c,
+        Term::Symb(_) => b,
         Term::Add(l) => map(l, Term::Add, eg),
         Term::Mul(l) => map(l, Term::Mul, eg),
         Term::Num(_) => b,
@@ -61,7 +61,7 @@ fn substitute(v: Id, b: Id, c: Id, eg: &mut EGraph<Term, ()>, touched: &mut Vec<
 }
 
 impl Applier<Term, ()> for BetaReduction {
-    fn apply_one(&self, eg: &mut EGraph<Term, ()>, id: Id, subst: &Subst, pat: Option<&PatternAst<Term>>, _rule_name: Symbol) -> Vec<Id> {
+    fn apply_one(&self, eg: &mut EGraph<Term, ()>, id: Id, subst: &Subst, _pat: Option<&PatternAst<Term>>, _rule_name: Symbol) -> Vec<Id> {
         let v: Var = "?v".parse().unwrap();
         let b: Var = "?b".parse().unwrap();
         let c: Var = "?c".parse().unwrap();
@@ -76,31 +76,4 @@ impl Applier<Term, ()> for BetaReduction {
 
         touched
     }
-}
-
-fn simplify(s: &str) -> String {
-    let expr: RecExpr<Term> = s.parse().unwrap();
-    let runner = Runner::default().with_expr(&expr).run(&make_rules());
-    let root = runner.roots[0];
-    let extractor = Extractor::new(&runner.egraph, AstSize);
-    let (best_cost, best) = extractor.find_best(root);
-    println!("Simplified {} to {} with cost {}", expr, best, best_cost);
-    best.to_string()
-}
-
-pub fn main() {
-    assert_eq!(simplify("(app (lam v b) c)"), "b");
-    assert_eq!(simplify("(app (lam v v) c)"), "c");
-
-    let omega = "(lam x (app x x))";
-    let infinite_loop = format!("(app {omega} {omega})");
-
-    let id = "(lam x x)";
-    let t = "(lam x (lam y x))";
-    let s = format!("(app (app {t} {id}) {infinite_loop})");
-
-    assert_eq!(simplify(&s), "(lam x x)");
-    assert_eq!(simplify(&infinite_loop), infinite_loop);
-
-    assert_eq!(simplify("(+ x (* 2 0))"), "x");
 }
