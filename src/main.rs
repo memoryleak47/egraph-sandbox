9897c2a828d4cc98b66a0905a156778504683103
memoryleak47 <memoryleak47@protonmail.ch>
2024-02-20 19:52:23 +0100

    add Y combinator example to test suite

--- src/main.rs
+++ src/main.rs
@@ -56,6 +56,23 @@ fn main() {
     let p = "(app (lam x (lam y x)) y)";
     let p = format!("(app {p} a)");
     assert_eq!(simplify(&p), "y");
+
+    // Y-combinator example.
+    // translating church numerals to numbers.
+    let a = "(lam x (app f (app x x)))";
+    let y = format!("(lam f (app {a} {a}))");
+
+    let zero = "(lam z (lam s z))";
+    let suc = "(lam arg (lam z (lam s (app s arg))))";
+
+    let d = "(lam a (+ (app f a) 1))";
+    let translate_impl = format!("(lam f (lam n (app (app n 0) {d})))");
+    let translate = format!("(lam n (app {y} {translate_impl}))");
+
+    // the number 2.
+    let rhs = format!("(app {suc} (app {suc} {zero}))");
+    let s = format!("(app {translate} {rhs})");
+    assert_eq!(simplify(&s), "(+ 1 1)");
 }
 
 fn simplify(s: &str) -> String {
