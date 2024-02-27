d7b1859e169338d440476485337d539ba94e98d5
memoryleak47 <memoryleak47@protonmail.ch>
2024-02-27 14:24:47 +0100

    ~to_ast

--- 3-miniegg-with-slots/src/syntax.rs
+++ 3-miniegg-with-slots/src/syntax.rs
@@ -53,18 +53,46 @@ fn translate(ast: Ast, re: &mut RecExpr) -> (AppliedId, NameMap) {
 
 ///// to_string
 
-fn to_ast(re: RecExpr, name_map: NameMap) -> Ast {
-    todo!()
+fn to_ast(re: &[ENode], mut name_map: HashMap<Slot, String>, namegen: &mut impl FnMut() -> String) -> Ast {
+    let n = re.last().unwrap();
+    match n {
+        ENode::Lam(x, b) => {
+            let xname = namegen();
+            name_map.insert(*x, xname.clone());
+
+            let b = to_ast(&re[0..b.id.0+1], name_map, namegen);
+
+            Ast::Lam(xname, Box::new(b))
+        },
+        ENode::App(l, r) => {
+            let l = to_ast(&re[0..l.id.0+1], name_map.clone(), namegen);
+            let r = to_ast(&re[0..r.id.0+1], name_map, namegen);
+
+            Ast::App(Box::new(l), Box::new(r))
+        },
+        ENode::Var(x) => {
+            let name = name_map[&x].clone();
+            Ast::Var(name)
+        },
+    }
 }
 
 pub fn to_string(re: RecExpr, name_map: NameMap) -> String {
-    let ast = to_ast(re, name_map);
+    let mut name_id = 0;
+    let mut namegen = || {
+        name_id += 1;
+
+        format!("x{name_id}")
+    };
+
+    let name_map: HashMap<Slot, String> = name_map.into_iter().map(|(x, y)| (y, x)).collect();
+    let ast = to_ast(&re.node_dag, name_map, &mut namegen);
     ast_to_string(ast)
 }
 
 #[test]
 fn test_parse_roundtrip() {
-    let s1 = "(app (lam x x) (lam y y))";
+    let s1 = "(app (lam x0 x0) (lam x1 x1))";
     let (p, m) = parse(s1);
     let s2 = to_string(p, m);
     assert_eq!(s1, s2);
