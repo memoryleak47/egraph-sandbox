3ce9e04d1dda8b3ae07519105f94d11b1798a9f3
memoryleak47 <memoryleak47@protonmail.ch>
2024-03-22 17:17:34 +0100

    ~5: beta

--- 5-egg-de-bruijn/src/beta.rs
+++ 5-egg-de-bruijn/src/beta.rs
@@ -1,5 +1,26 @@
 use crate::*;
 
 pub fn beta_reduction() -> Rewrite<ENode, ()> {
+    rewrite!("beta-reduction"; "(app (lam ?b) ?t)" => { BetaReduction })
+}
+
+struct BetaReduction;
+
+impl Applier<ENode, ()> for BetaReduction {
+    fn apply_one(&self, eg: &mut EG, id: Id, subst: &Subst, _pat: Option<&PatternAst<ENode>>, _rule_name: Symbol) -> Vec<Id> {
+        let b: Var = "?b".parse().unwrap();
+        let t: Var = "?t".parse().unwrap();
+
+        let b: Id = subst[b];
+        let t: Id = subst[t];
+
+        let new = substitution(b, t, eg);
+        eg.union(new, id);
+
+        Vec::new() // is this fine?
+    }
+}
+
+fn substitution(b: Id, t: Id, eg: &mut EG) -> Id {
     todo!()
 }
