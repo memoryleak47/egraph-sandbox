1cef33d181760d5234a4d5020f26ac7bc22ae712
memoryleak47 <memoryleak47@protonmail.ch>
2024-04-09 14:05:32 +0200

    make extract generic over Language

--- 3-miniegg-with-slots/src/extract.rs
+++ 3-miniegg-with-slots/src/extract.rs
@@ -3,13 +3,13 @@ use crate::*;
 // our cost function is RecExpr::node_dag.len(), and we build every RecExpr s.t. each element of the node DAG is used exactly once.
 // This is hence equivalent to AST size.
 // `i` is not allowed to have free variables, hence prefer `Id` over `AppliedId`.
-pub fn extract(i: Id, eg: &EGraph<ENode>) -> RecExpr<ENode> {
+pub fn extract<L: Language>(i: Id, eg: &EGraph<L>) -> RecExpr<L> {
     let i = eg.find_id(i);
 
     // this is a terribly slow algorithm.
 
     // maps eclass id to their optimal RecExpr.
-    let mut map: HashMap<Id, RecExpr<ENode>> = HashMap::default();
+    let mut map: HashMap<Id, RecExpr<L>> = HashMap::default();
 
     for _ in 0..eg.ids().len() {
         for id in eg.ids() {
@@ -31,7 +31,7 @@ pub fn extract(i: Id, eg: &EGraph<ENode>) -> RecExpr<ENode> {
     map.remove(&i).unwrap()
 }
 
-fn extract_step(enode: ENode, map: &HashMap<Id, RecExpr<ENode>>) -> Option<RecExpr<ENode>> {
+fn extract_step<L: Language>(enode: L, map: &HashMap<Id, RecExpr<L>>) -> Option<RecExpr<L>> {
     let mut c = enode.clone();
     let mut re = RecExpr::empty();
     for x in c.applied_id_occurences_mut() {
