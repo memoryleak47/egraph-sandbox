c4d09200d0dfd59cdeb0ea8fe3ddcb4b5ce50484
memoryleak47 <memoryleak47@protonmail.ch>
2024-04-12 10:49:18 +0200

    add minified baseline

---
+++ 7-baseline-explicit-subst-minified/src/cost.rs
@@ -0,0 +1,54 @@
+
+use crate::*;
+
+use std::cmp::Ordering;
+
+// only returns valid lambda calculus terms, nothing else.
+pub struct RestrictedAstSize;
+
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub enum MyCost {
+    Finite(u32),
+    Infinite,
+}
+
+fn add(a: MyCost, b: MyCost) -> MyCost {
+    use MyCost::*;
+    match (a, b) {
+        (Infinite, _) => Infinite,
+        (_, Infinite) => Infinite,
+        (Finite(x), Finite(y)) => Finite(x + y),
+    }
+}
+
+fn add1(a: MyCost, b: MyCost) -> MyCost {
+    add(add(a, b), MyCost::Finite(1))
+}
+
+impl PartialOrd for MyCost {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        use MyCost::*;
+        match (self, other) {
+            (Finite(x), Finite(y)) => x.partial_cmp(y),
+            (Infinite, Infinite) => Some(Ordering::Equal),
+            (Infinite, Finite(_)) => Some(Ordering::Greater),
+            (Finite(_), Infinite) => Some(Ordering::Less),
+        }
+    }
+}
+
+impl CostFunction<Lambda> for RestrictedAstSize {
+    type Cost = MyCost;
+
+    fn cost<C>(&mut self, t: &Lambda, mut costs: C) -> MyCost where C: FnMut(Id) -> MyCost {
+        match t {
+            Lambda::Lambda([_, b]) => add(costs(*b), MyCost::Finite(1)),
+            Lambda::Var(i) => add(costs(*i), MyCost::Finite(1)),
+            Lambda::App([l, r]) => add1(costs(*l), costs(*r)),
+            Lambda::Symbol(_) => MyCost::Finite(0),
+            _ => MyCost::Infinite,
+        }
+    }
+}
+
+
---
+++ 7-baseline-explicit-subst-minified/src/lambda.rs
@@ -0,0 +1,119 @@
+use egg::{rewrite as rw, *};
+use fxhash::FxHashSet as HashSet;
+
+define_language! {
+    pub enum Lambda {
+        "var" = Var(Id),
+
+        "app" = App([Id; 2]),
+        "lam" = Lambda([Id; 2]),
+        "let" = Let([Id; 3]),
+
+        Symbol(egg::Symbol),
+    }
+}
+
+type EGraph = egg::EGraph<Lambda, LambdaAnalysis>;
+
+#[derive(Default)]
+pub struct LambdaAnalysis;
+
+#[derive(Debug)]
+pub struct Data {
+    free: HashSet<Id>,
+}
+
+impl Analysis<Lambda> for LambdaAnalysis {
+    type Data = Data;
+    fn merge(&mut self, to: &mut Data, from: Data) -> DidMerge {
+        let before_len = to.free.len();
+        // to.free.extend(from.free);
+        to.free.retain(|i| from.free.contains(i));
+        // compare lengths to see if I changed to or from
+        DidMerge(
+            before_len != to.free.len(),
+            to.free.len() != from.free.len(),
+        )
+    }
+
+    fn make(egraph: &EGraph, enode: &Lambda) -> Data {
+        let f = |i: &Id| egraph[*i].data.free.iter().cloned();
+        let mut free = HashSet::default();
+        match enode {
+            Lambda::Var(v) => {
+                free.insert(*v);
+            }
+            Lambda::Let([v, a, b]) => {
+                free.extend(f(b));
+                free.remove(v);
+                free.extend(f(a));
+            }
+            Lambda::Lambda([v, a]) => {
+                free.extend(f(a));
+                free.remove(v);
+            }
+            _ => enode.for_each(|c| free.extend(&egraph[c].data.free)),
+        }
+        Data { free }
+    }
+}
+
+fn var(s: &str) -> Var {
+    s.parse().unwrap()
+}
+
+fn is_not_same_var(v1: Var, v2: Var) -> impl Fn(&mut EGraph, Id, &Subst) -> bool {
+    move |egraph, _, subst| egraph.find(subst[v1]) != egraph.find(subst[v2])
+}
+
+pub fn rules() -> Vec<Rewrite<Lambda, LambdaAnalysis>> {
+    vec![
+        rw!("beta";     "(app (lam ?v ?body) ?e)" => "(let ?v ?e ?body)"),
+        rw!("let-app";  "(let ?v ?e (app ?a ?b))" => "(app (let ?v ?e ?a) (let ?v ?e ?b))"),
+        rw!("let-var-same"; "(let ?v1 ?e (var ?v1))" => "?e"),
+        rw!("let-var-diff"; "(let ?v1 ?e (var ?v2))" => "(var ?v2)"
+            if is_not_same_var(var("?v1"), var("?v2"))),
+        rw!("let-lam-same"; "(let ?v1 ?e (lam ?v1 ?body))" => "(lam ?v1 ?body)"),
+        rw!("let-lam-diff";
+            "(let ?v1 ?e (lam ?v2 ?body))" =>
+            { CaptureAvoid {
+                fresh: var("?fresh"), v2: var("?v2"), e: var("?e"),
+                if_not_free: "(lam ?v2 (let ?v1 ?e ?body))".parse().unwrap(),
+                if_free: "(lam ?fresh (let ?v1 ?e (let ?v2 (var ?fresh) ?body)))".parse().unwrap(),
+            }}
+            if is_not_same_var(var("?v1"), var("?v2"))),
+    ]
+}
+
+struct CaptureAvoid {
+    fresh: Var,
+    v2: Var,
+    e: Var,
+    if_not_free: Pattern<Lambda>,
+    if_free: Pattern<Lambda>,
+}
+
+impl Applier<Lambda, LambdaAnalysis> for CaptureAvoid {
+    fn apply_one(
+        &self,
+        egraph: &mut EGraph,
+        eclass: Id,
+        subst: &Subst,
+        searcher_ast: Option<&PatternAst<Lambda>>,
+        rule_name: Symbol,
+    ) -> Vec<Id> {
+        let e = subst[self.e];
+        let v2 = subst[self.v2];
+        let v2_free_in_e = egraph[e].data.free.contains(&v2);
+        if v2_free_in_e {
+            let mut subst = subst.clone();
+            let sym = Lambda::Symbol(format!("_{}", eclass).into());
+            subst.insert(self.fresh, egraph.add(sym));
+            self.if_free
+                .apply_one(egraph, eclass, &subst, searcher_ast, rule_name)
+        } else {
+            self.if_not_free
+                .apply_one(egraph, eclass, subst, searcher_ast, rule_name)
+        }
+    }
+}
