9dadd6e7dc043cd6f3827c8e9c822f74e592530a
memoryleak47 <memoryleak47@protonmail.ch>
2024-04-16 19:18:39 +0200

    ...

--- 3-miniegg-with-slots/src/pattern/ematch.rs
+++ 3-miniegg-with-slots/src/pattern/ematch.rs
@@ -1,9 +1,18 @@
 use crate::*;
 
 struct Match {
-    
+    id: Id,
+    subst: HashMap<String, AppliedId>,
 }
 
 pub fn ematch<L: Language>(eg: &EGraph<L>, pattern: &Pattern<L>) -> Vec<Match> {
+    let mut out = Vec::new();
+    for id in eg.ids() {
+        out.extend(ematch_impl(eg, pattern, id, HashMap::default()));
+    }
+    out
+}
+
+fn ematch_impl<L: Language>(eg: &EGraph<L>, pattern: &Pattern<L>, id: Id, partial_subst: HashMap<String, AppliedId>) -> Vec<Match> {
     todo!()
 }
