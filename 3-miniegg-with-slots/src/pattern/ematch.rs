a1bf253f6e11eed8296f9ef7342e49aabdaed859
memoryleak47 <memoryleak47@protonmail.ch>
2024-04-18 17:38:35 +0200

    +superficial_match

--- 3-miniegg-with-slots/src/pattern/ematch.rs
+++ 3-miniegg-with-slots/src/pattern/ematch.rs
@@ -23,3 +23,19 @@ fn ematch_impl<L: Language>(eg: &EGraph<L>, pattern: &Pattern<L>, id: Id, partia
         },
     }
 }
+
+fn superficial_match<L: Language>(a: &L, b: &L) -> bool {
+    return mk_superficial(a) == mk_superficial(b);
+
+    fn mk_superficial<L: Language>(a: &L) -> L {
+        let mut a = a.clone();
+        for x in a.applied_id_occurences_mut() {
+            *x = AppliedId::new(Id(0), SlotMap::new());
+        }
+        for x in a.all_slot_occurences_mut() {
+            *x = Slot(0);
+        }
+
+        a
+    }
+}
