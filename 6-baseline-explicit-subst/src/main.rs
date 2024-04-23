0985758ddabd07903d31f1bc7b30b9aa21298a66
memoryleak47 <memoryleak47@protonmail.ch>
2024-04-23 18:27:05 +0200

    ... apply same translation for 6-baseline

--- 6-baseline-explicit-subst/src/main.rs
+++ 6-baseline-explicit-subst/src/main.rs
@@ -7,90 +7,11 @@ use lambda::*;
 mod cost;
 use cost::*;
 
-struct Expr(RecExpr<Lambda>);
+mod real;
+use real::*;
 
-impl Realization for Expr {
-    type Id = Id;
-
-    fn to_ast_string(&self) -> String {
-        let mut strings = Vec::new();
-        for n in self.0.as_ref() {
-            strings.push(match n {
-                Lambda::Lambda([x, b]) => format!("(lam {} {})", &strings[usize::from(*x)], &strings[usize::from(*b)]),
-                Lambda::App([l, r]) => format!("(app {} {})", &strings[usize::from(*l)], &strings[usize::from(*r)]),
-                Lambda::Var(v) => format!("{}", &strings[usize::from(*v)]),
-                Lambda::Symbol(s) => format!("{}", s),
-                _ => panic!(),
-            });
-        }
-
-        strings.pop().unwrap()
-    }
-
-    fn from_ast(a: &Ast) -> Self {
-        let re: RecExpr<Lambda> = to_string(a).parse().unwrap();
-        return Self(re);
-
-        fn to_string(a: &Ast) -> String {
-            match a {
-                Ast::Var(x) => format!("(var {})", x),
-                Ast::Lam(x, b) => format!("(lam {} {})", &x, to_string(&*b)),
-                Ast::App(l, r) => format!("(app {} {})", to_string(&*l), to_string(&*r)),
-            }
-        }
-    }
-    
-    fn simplify(&self, steps: u32) -> Self {
-        let rewrites = rules();
-
-        let h = |r: &mut Runner<_, _>| {
-            // println!("{}", r.egraph.total_size());
-            Ok(())
-        };
-        let runner = Runner::default()
-                                    .with_iter_limit(steps as usize)
-                                    .with_scheduler(SimpleScheduler)
-                                    .with_node_limit(10000000000000)
-                                    .with_time_limit(std::time::Duration::from_secs(60*60))
-                                    .with_expr(&self.0)
-                                    .with_hook(h)
-                                    .run(&rewrites);
-        // println!("last: {}", runner.egraph.total_size());
-
-        let extr = Extractor::new(&runner.egraph, RestrictedAstSize);
-        let (_, out) = extr.find_best(runner.roots[0]);
-
-        Self(out)
-    }
-
-    fn find_eq(&self, other: &Self, steps: u32) -> bool {
-        let rewrites = rules();
-        let mut eg = EGraph::default();
-
-        let i1 = eg.add_expr(&self.0);
-        let i2 = eg.add_expr(&other.0);
-        
-        let h = |r: &mut Runner<_, _>| {
-            // println!("{}", r.egraph.total_size());
-            Ok(())
-        };
-        let runner = Runner::default()
-                                    .with_iter_limit(steps as usize)
-                                    .with_scheduler(SimpleScheduler)
-                                    .with_node_limit(10000000000000)
-                                    .with_time_limit(std::time::Duration::from_secs(60*60))
-                                    .with_egraph(eg)
-                                    .with_hook(h)
-                                    .run(&rewrites);
-
-        runner.egraph.find(i1) == runner.egraph.find(i2)
-    }
-}
-
-unpack_tests!(Expr);
 
 fn main() {
     let s = app(app(add(), num(2)), num(2));
-    check_simplify::<Expr>(&s, 24);
-
+    check_simplify::<LambdaReal>(&s);
 }
---
+++ 6-baseline-explicit-subst/src/real.rs
@@ -0,0 +1,81 @@
+use crate::*;
+
+pub struct LambdaReal(EGraph<Lambda, LambdaAnalysis>);
+
+impl Realization for LambdaReal {
+    type Id = Id;
+
+    fn new() -> Self {
+        LambdaReal(EGraph::default())
+    }
+
+    fn add_ast(&mut self, ast: &Ast) -> Self::Id {
+        let re = re_from_ast(ast);
+        self.0.add_expr(&re)
+    }
+
+    fn extract_ast(&self, id: Self::Id) -> Ast {
+        let extr = Extractor::new(&self.0, RestrictedAstSize);
+        let (_, out) = extr.find_best(id);
+
+        re_to_ast(&out)
+    }
+
+    fn find(&self, id: Self::Id) -> Self::Id {
+        self.0.find(id)
+    }
+
+    fn step(&mut self) {
+        let mut eg2 = EGraph::default();
+        std::mem::swap(&mut eg2, &mut self.0);
+        let mut r = Runner::default().with_egraph(eg2).with_iter_limit(1).run(&rules());
+        std::mem::swap(&mut r.egraph, &mut self.0);
+    }
+
+    fn enode_count(&self) -> usize { self.0.total_size() }
+    fn eclass_count(&self) -> usize { self.0.classes().filter(|x| x.id == self.find(x.id)).count() }
+}
+
+fn varcount(eg: &EGraph<Lambda, LambdaAnalysis>) -> usize {
+    use std::collections::HashSet;
+
+    let mut hashset: HashSet<&Symbol> = HashSet::default();
+    for c in eg.classes() {
+        for i in c.iter() {
+            if let Lambda::Symbol(v) = i {
+                hashset.insert(v);
+            }
+        }
+    }
+
+    hashset.len()
+}
+
+fn re_to_ast(re: &RecExpr<Lambda>)-> Ast {
+    let mut strings = Vec::new();
+    for n in re.as_ref() {
+        strings.push(match n {
+            Lambda::Lambda([x, b]) => format!("(lam {} {})", &strings[usize::from(*x)], &strings[usize::from(*b)]),
+            Lambda::App([l, r]) => format!("(app {} {})", &strings[usize::from(*l)], &strings[usize::from(*r)]),
+            Lambda::Var(v) => format!("{}", &strings[usize::from(*v)]),
+            Lambda::Symbol(s) => format!("{}", s),
+            _ => panic!(),
+        });
+    }
+
+    Ast::parse(strings.last().unwrap())
+}
+
+fn re_from_ast(a: &Ast) -> RecExpr<Lambda> {
+    return to_string(a).parse().unwrap();
+
+    fn to_string(a: &Ast) -> String {
+        match a {
+            Ast::Var(x) => format!("(var {})", x),
+            Ast::Lam(x, b) => format!("(lam {} {})", &x, to_string(&*b)),
+            Ast::App(l, r) => format!("(app {} {})", to_string(&*l), to_string(&*r)),
+        }
+    }
+}
+    
+unpack_tests!(LambdaReal);
