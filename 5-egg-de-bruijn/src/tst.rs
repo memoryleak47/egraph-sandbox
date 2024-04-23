78f3b218c7405cc230081f4e645ec5dc78800a4f
memoryleak47 <memoryleak47@protonmail.ch>
2024-04-23 19:14:44 +0200

    fixup 5-egg-de-bruijn Realization to the new standard

--- 5-egg-de-bruijn/src/tst.rs
+++ 5-egg-de-bruijn/src/tst.rs
@@ -1,41 +1,43 @@
 use crate::*;
 
-pub struct Expr(RecExpr<ENode>);
+pub struct DeBruijnReal(EGraph<ENode, Varbound>);
 
-impl Realization for Expr {
-    fn to_ast_string(&self) -> String {
-        de_bruijn_to_named(&self.0.to_string())
+impl Realization for DeBruijnReal {
+    type Id = Id;
+
+    fn new() -> Self {
+        DeBruijnReal(EGraph::default())
     }
 
-    fn from_ast(ast: &Ast) -> Self {
-        let s = ast.to_string();
-        let s = named_to_de_bruijn(&s);
-        let s: RecExpr<ENode> = s.parse().unwrap();
-        let s = Expr(s);
-        s
+    fn add_ast(&mut self, ast: &Ast) -> Self::Id {
+        let s = named_to_de_bruijn(&ast.to_string());
+        let re = s.parse().unwrap();
+        self.0.add_expr(&re)
     }
 
-    fn simplify(&self, steps: u32) -> Self {
-        let rewrites = [beta_reduction()];
-        let runner = Runner::default().with_scheduler(SimpleScheduler).with_iter_limit(steps as usize).with_expr(&self.0).run(&rewrites);
+    fn extract_ast(&self, id: Self::Id) -> Ast {
+        let extr = Extractor::new(&self.0, MyAstSize);
+        let (_, out) = extr.find_best(id);
 
-        let extr = Extractor::new(&runner.egraph, MyAstSize);
-        let (_, out) = extr.find_best(runner.roots[0]);
-
-        Expr(out)
+        let s = de_bruijn_to_named(&out.to_string());
+        Ast::parse(&s)
     }
 
-    fn find_eq(&self, other: &Self, steps: u32) -> bool {
-        let mut eg = EG::new(Varbound);
-
-        let i1 = eg.add_expr(&self.0);
-        let i2 = eg.add_expr(&other.0);
-
-        let rewrites = [beta_reduction()];
-        let runner = Runner::default().with_scheduler(SimpleScheduler).with_iter_limit(steps as usize).with_egraph(eg).run(&rewrites);
-
-        runner.egraph.find(i1) == runner.egraph.find(i2)
+    fn find(&self, id: Self::Id) -> Self::Id {
+        self.0.find(id)
     }
+
+    fn step(&mut self) {
+        let rules = [beta_reduction()];
+
+        let mut eg2 = EGraph::default();
+        std::mem::swap(&mut eg2, &mut self.0);
+        let mut r = Runner::default().with_egraph(eg2).with_iter_limit(1).run(&rules);
+        std::mem::swap(&mut r.egraph, &mut self.0);
+    }
+
+    fn enode_count(&self) -> usize { self.0.total_size() }
+    fn eclass_count(&self) -> usize { self.0.classes().filter(|x| x.id == self.find(x.id)).count() }
 }
 
-unpack_tests!(Expr);
+unpack_tests!(DeBruijnReal);
