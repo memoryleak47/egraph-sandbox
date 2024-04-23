3746b62acfafe98550fe812a36f3ec49079b70db
memoryleak47 <memoryleak47@protonmail.ch>
2024-04-23 15:45:11 +0200

    [WIP] restructure trait Realization

--- lamcalc/src/realization.rs
+++ lamcalc/src/realization.rs
@@ -8,46 +8,53 @@ pub trait Realization: Sized {
     fn extract_ast(&self, id: Self::Id) -> Ast { todo!() }
     fn find(&self, id: Self::Id) -> Self::Id { todo!() }
     fn step(&mut self) { todo!() }
-
-    fn to_ast_string(&self) -> String;
-    fn from_ast(ast: &Ast) -> Self;
-    fn simplify(&self, steps: u32) -> Self;
-    fn find_eq(&self, other: &Self, steps: u32) -> bool;
-
-    fn to_ast(&self) -> Ast {
-        Ast::parse(&self.to_ast_string())
-    }
-
-    fn from_ast_string(s: &str) -> Self {
-        Self::from_ast(&Ast::parse(s))
-    }
+    fn enode_count(&self) -> usize { todo!() }
+    fn eclass_count(&self) -> usize { todo!() }
 }
 
-pub fn simplify<R: Realization>(s: &str, steps: u32) -> String {
-    let s = R::from_ast_string(s);
-    let s = s.simplify(steps);
-    let s = s.to_ast_string();
-    s
+pub fn simplify<R: Realization>(s: &str) -> String {
+    let ast = Ast::parse(s);
+    let mut eg = R::new();
+    let i = eg.add_ast(&ast);
+    for _ in 0..200 {
+        eg.step();
+        if eg.enode_count() > 40000 {
+            break;
+        }
+    }
+    let out = eg.extract_ast(i.clone());
+    let out = out.to_string();
+
+    out
 }
 
 // TODO the smallest term isn't necessarily the beta-NF.
-pub fn check_simplify<R: Realization>(p: &str, steps: u32) {
-    let out1 = simplify::<R>(p, steps);
+pub fn check_simplify<R: Realization>(p: &str) {
+    let out1 = simplify::<R>(p);
     let out2 = run(p);
     assert_alpha_eq(&*out1, &*out2);
 }
 
 // checks whether simplify has valid output, even though it might not be able to finish the whole computation.
-pub fn check_simplify_incomplete<R: Realization>(p: &str, steps: u32) {
-    let out1 = run(&simplify::<R>(p, steps));
+pub fn check_simplify_incomplete<R: Realization>(p: &str) {
+    let out1 = run(&simplify::<R>(p));
     let out2 = run(p);
     assert_alpha_eq(&*out1, &*out2);
 }
 
 pub fn check_eq<R: Realization>(s1: &str, s2: &str, steps: u32) {
-    let s1 = R::from_ast_string(s1);
-    let s2 = R::from_ast_string(s2);
-    assert!(R::find_eq(&s1, &s2, steps));
+    let s1 = Ast::parse(s1);
+    let s2 = Ast::parse(s2);
+    let mut eg = R::new();
+    let i1 = eg.add_ast(&s1);
+    let i2 = eg.add_ast(&s2);
+    for _ in 0..200 {
+        eg.step();
+        if eg.enode_count() > 40000 {
+            break;
+        }
+    }
+    assert!(eg.find(i1) == eg.find(i2));
 }
 
 // Non-Realization functions:
