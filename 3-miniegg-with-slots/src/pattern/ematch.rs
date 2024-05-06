bb9d1bf070530d29dcea474a4ea6b45b2bcb4d8a
memoryleak47 <memoryleak47@protonmail.ch>
2024-05-06 13:04:31 +0200

    +Traversal

--- 3-miniegg-with-slots/src/pattern/ematch.rs
+++ 3-miniegg-with-slots/src/pattern/ematch.rs
@@ -5,3 +5,13 @@ type Subst = HashMap<String, AppliedId>;
 pub fn ematch<L: Language>(eg: &EGraph<L>, pattern: &Pattern<L>) -> Vec<Subst> {
     todo!()
 }
+
+struct Traversal<L: Language> {
+    id: Id,
+    node: Option<TraversalNode<L>>,
+}
+
+struct TraversalNode<L: Language> {
+    shape: L,
+    children: Vec<Traversal<L>>,
+}
