f3e9d9a9c0d65e6f99fb2bc289420e2dc4f41b18
memoryleak47 <memoryleak47@protonmail.ch>
2024-05-06 13:10:12 +0200

    ~ematch

--- 3-miniegg-with-slots/src/pattern/ematch.rs
+++ 3-miniegg-with-slots/src/pattern/ematch.rs
@@ -3,7 +3,23 @@ use crate::*;
 type Subst = HashMap<String, AppliedId>;
 
 pub fn ematch<L: Language>(eg: &EGraph<L>, pattern: &Pattern<L>) -> Vec<Subst> {
-    todo!()
+    let mut out = Vec::new();
+    for i in eg.ids() {
+        // invariant: each x in worklist satisfies x.compatible(pattern)
+        let mut worklist = vec![Traversal::new(i)];
+        while let Some(x) = worklist.pop() {
+            if let Some(xs) = x.branch(pattern) {
+                for y in xs {
+                    if y.compatible(pattern) {
+                        worklist.push(y);
+                    }
+                }
+            } else {
+                out.push(x.to_subst(pattern));
+            }
+        }
+    }
+    out
 }
 
 struct Traversal<L: Language> {
@@ -14,4 +30,25 @@ struct Traversal<L: Language> {
 struct TraversalNode<L: Language> {
     shape: L,
     children: Vec<Traversal<L>>,
+}
+
+impl<L: Language> Traversal<L> {
+    pub fn new(id: Id) -> Self {
+        Traversal {
+            id,
+            node: None,
+        }
+    }
+
+    pub fn branch(&self, pattern: &Pattern<L>) -> Option<Vec<Self>> {
+        todo!()
+    }
+
+    pub fn compatible(&self, pattern: &Pattern<L>) -> bool {
+        todo!()
+    }
+
+    pub fn to_subst(&self, pattern: &Pattern<L>) -> Subst {
+        todo!()
+    }
 }
