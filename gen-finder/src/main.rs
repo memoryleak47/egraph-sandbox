f80147892670c909029b10c41ee22d3d3d94bb8f
memoryleak47 <memoryleak47@protonmail.ch>
2024-05-10 00:11:15 +0200

    fixup gen-finder

--- gen-finder/src/main.rs
+++ gen-finder/src/main.rs
@@ -1,8 +1,8 @@
 extern crate miniegg_with_slots;
 extern crate explicit_subst;
 
-use miniegg_with_slots::LetExpr;
-use explicit_subst::Expr;
+use miniegg_with_slots::LetReal;
+use explicit_subst::LambdaRealImproved;
 use lamcalc::*;
 
 fn main() {
@@ -10,8 +10,8 @@ fn main() {
     let g = "(app (app (app (lam b (lam b (app b (lam z b)))) (app (lam z z) (lam c c))) (lam y (app y (app y (lam b b))))) (lam c (lam y (app (lam b (app b (lam y (app (lam c c) c)))) (lam c (app (lam c (lam x x)) (lam a (lam a (app a (lam y a))))))))))";
     let g = &format!("(app {} {})", g, g);
     dbg!(&g);
-    let a = simplify::<LetExpr>(&g, 20);
+    let a = simplify::<LetReal>(&g);
     dbg!(&a);
-    let b = simplify::<Expr>(&g, 15); // It runs out of memory at 16 iterations.
+    let b = simplify::<LambdaRealImproved>(&g);
     dbg!(&b);
 }
