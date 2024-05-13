1c3173c558ffefb79784bb23a28b42d7e1bde55f
memoryleak47 <memoryleak47@protonmail.ch>
2024-05-13 15:56:49 +0200

    start with binomial skeleton

--- 3-miniegg-with-slots/src/i_rise/tst.rs
+++ 3-miniegg-with-slots/src/i_rise/tst.rs
@@ -76,12 +76,36 @@ fn fission_re2() -> RecExpr2<RiseENode> {
 }
 
 #[test]
-pub fn test_fission() {
+fn test_fission() {
     let mut eg = EGraph::new();
     let i1 = add_rec_expr2(&fission_re1(), &mut eg);
     for _ in 0..40 {
         rewrite_rise(&mut eg);
         if let Some(i2) = lookup_rec_expr2(&fission_re2(), &eg) {
+            assert_eq!(eg.find_id(i1.id), eg.find_id(i2.id));
+            return;
+        }
+    }
+
+    assert!(false);
+}
+
+// BINOMIAL //
+
+fn binomial_re1() -> RecExpr2<RiseENode> {
+    todo!()
+}
+
+fn binomial_re2() -> RecExpr2<RiseENode> {
+    todo!()
+}
+
+pub fn test_binomial() {
+    let mut eg = EGraph::new();
+    let i1 = add_rec_expr2(&binomial_re1(), &mut eg);
+    for _ in 0..40 {
+        rewrite_rise(&mut eg);
+        if let Some(i2) = lookup_rec_expr2(&binomial_re2(), &eg) {
             assert_eq!(eg.find_id(i1.id), eg.find_id(i2.id));
             return;
         }
