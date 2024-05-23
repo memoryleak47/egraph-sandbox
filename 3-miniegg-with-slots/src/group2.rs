2490cf5bce54c3b9ffc54ca368b7a3a4f9a19b1a
memoryleak47 <memoryleak47@protonmail.ch>
2024-05-23 10:46:05 +0200

    finish first sims-schreier impl

--- 3-miniegg-with-slots/src/group2.rs
+++ 3-miniegg-with-slots/src/group2.rs
@@ -1,5 +1,9 @@
 use crate::*;
 
+// In order to be compatible with the literature:
+// https://en.wikipedia.org/wiki/Schreier%27s_lemma
+// I define "x y" = x.compose(y)
+
 // Trivial implementation of permutation Group.
 #[derive(Clone, Debug)]
 pub struct Group {
@@ -27,12 +31,44 @@ impl Group {
                     .map(|s| Box::new(Next::new(s, &omega, generators)));
         Group { omega, next }
     }
+
+    pub fn generators(&self) -> HashSet<Perm> {
+        match &self.next {
+            None => [Perm::identity(&self.omega)].into_iter().collect(),
+            Some(n) => &n.ot.values().cloned().collect::<HashSet<_>>() | &n.g.generators(),
+        }
+    }
+
+    pub fn all_perms(&self) -> HashSet<Perm> {
+        match &self.next {
+            None => [Perm::identity(&self.omega)].into_iter().collect(),
+            Some(n) => {
+                let mut out = HashSet::default();
+
+                let left = n.ot.values().cloned().collect::<HashSet<_>>();
+                let right = n.g.all_perms();
+
+                for l in &left {
+                    for r in &right {
+                        // TODO correct order?
+                        out.insert(l.compose(r));
+                    }
+                }
+                out
+            }
+        }
+    }
+
+    // TODO improve.
+    pub fn contains(&self, p: &Perm) -> bool {
+        self.all_perms().contains(p)
+    }
 }
 
 impl Next {
     fn new(stab: Slot, omega: &HashSet<Slot>, generators: HashSet<Perm>) -> Self {
         let ot = build_ot(stab, omega, &generators);
-        let generators = schreiers_lemma(generators);
+        let generators = schreiers_lemma(stab, &ot, generators);
         let g = Group::new(omega, generators);
         Next { stab, ot, g }
     }
@@ -63,8 +99,16 @@ fn build_ot(stab: Slot, omega: &HashSet<Slot>, generators: &HashSet<Perm>) -> Ha
 }
 
 // extends the set of generators using Schreiers Lemma.
-fn schreiers_lemma(generators: HashSet<Perm>) -> HashSet<Perm> {
-    todo!()
+fn schreiers_lemma(stab: Slot, ot: &HashMap<Slot, Perm>, generators: HashSet<Perm>) -> HashSet<Perm> {
+    let mut out = HashSet::default();
+    for (_, r) in ot {
+        for s in &generators {
+            let rs = r.compose(s);
+            let rs2_inv = ot[&rs[stab]].inverse();
+            out.insert(rs.compose(&rs2_inv));
+        }
+    }
+    out
 }
 
 // finds the lowest Slot that's not stabilized in at least one of the generators.
@@ -81,4 +125,17 @@ fn find_lowest_nonstab(generators: &HashSet<Perm>) -> Option<Slot> {
         }
     }
     min
+}
+
+#[test]
+fn group_test1() {
+    let s = Slot::new;
+    let [s0, s1, s2, s3] = [s(0), s(1), s(2), s(3)];
+    let omega = [s0, s1, s2, s3].into_iter().collect();
+
+    let perm1: Perm = [(s0, s1), (s1, s0), (s2, s2), (s3, s3)].into_iter().collect();
+    let perm2: Perm = [(s0, s0), (s1, s1), (s2, s3), (s3, s2)].into_iter().collect();
+    let g1 = Group::new(&omega, [&perm1, &perm2].into_iter().cloned().collect());
+    dbg!(g1.all_perms());
+    assert!(false);
 }
