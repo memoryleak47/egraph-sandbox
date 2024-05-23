3acdd824aa649765fa50a4fca75e9d8b0ec630f7
memoryleak47 <memoryleak47@protonmail.ch>
2024-05-23 16:19:54 +0200

    cleared redundant main.rs

--- 3-miniegg-with-slots/src/main.rs
+++ 3-miniegg-with-slots/src/main.rs
@@ -1,48 +1,4 @@
-use symbol_table::GlobalSymbol as Symbol;
-use lamcalc::{Realization, Ast};
-
-mod types;
-use types::*;
-
-mod lang;
-use lang::*;
-
-mod i_lambda;
-use i_lambda::*;
-
-mod i_let;
-use i_let::*;
-
-mod i_rise;
-use i_rise::*;
-
-mod i_arith;
-use i_arith::*;
-
-mod syntax;
-use syntax::*;
-
-mod slotmap;
-use slotmap::*;
-
-mod debug;
-
-mod egraph;
-use egraph::*;
-
-mod extract;
-use extract::*;
-
-mod pattern;
-use pattern::*;
-
-mod group;
-use group::*;
-
-use std::hash::Hash;
-use std::fmt::Debug;
-pub type HashMap<K, V> = fnv::FnvHashMap<K, V>;
-pub type HashSet<T> = fnv::FnvHashSet<T>;
+use miniegg_with_slots::*;
 
 fn main() {
 }
