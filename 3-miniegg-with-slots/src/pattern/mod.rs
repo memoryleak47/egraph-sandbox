85710d2e773ce5f623395f05059e42c07912396b
memoryleak47 <memoryleak47@protonmail.ch>
2024-06-07 11:41:03 +0200

    add dedicated rewrite mod

--- 3-miniegg-with-slots/src/pattern/mod.rs
+++ 3-miniegg-with-slots/src/pattern/mod.rs
@@ -9,22 +9,5 @@ pub use ematch::*;
 mod pattern_subst;
 pub use pattern_subst::*;
 
-
-pub fn rewrite_if<L: Language>(eg: &mut EGraph<L>, a: Pattern<L>, b: Pattern<L>, cond: impl Fn(&Subst) -> bool) {
-    for subst in ematch_all(eg, &a) {
-        if cond(&subst) {
-            let a = pattern_subst(eg, &a, &subst);
-            let b = pattern_subst(eg, &b, &subst);
-            eg.union(&a, &b);
-        }
-    }
-}
-
-pub fn rewrite<L: Language>(eg: &mut EGraph<L>, a: Pattern<L>, b: Pattern<L>) {
-    rewrite_if(eg, a, b, |_| true);
-}
-
-pub fn rewrite_bi<L: Language>(eg: &mut EGraph<L>, a: Pattern<L>, b: Pattern<L>) {
-    rewrite(eg, a.clone(), b.clone());
-    rewrite(eg, b, a);
-}
+mod rewrite;
+pub use rewrite::*;
---
+++ 3-miniegg-with-slots/src/pattern/rewrite.rs
@@ -0,0 +1,57 @@
+use crate::*;
+
+pub struct Rewrite<L: Language> {
+    searcher: Pattern<L>,
+    applier: Box<dyn Fn(Subst, &mut EGraph<L>)>,
+}
+
+pub fn do_rewrites<L: Language>(eg: &mut EGraph<L>, rewrites: &[Rewrite<L>]) {
+    let matches: Vec<_> = rewrites.iter().map(|rw| ematch_all(eg, &rw.searcher)).collect();
+    for (rw, mtchs) in rewrites.iter().zip(matches.into_iter()) {
+        for subst in mtchs {
+            (*rw.applier)(subst, eg);
+        }
+    }
+}
+
+
+// Indirect rewrites.
+
+pub fn mk_rewrite_if<L: Language + 'static>(eg: &mut EGraph<L>, a: Pattern<L>, b: Pattern<L>, cond: impl Fn(&Subst) -> bool + 'static) -> Rewrite<L> {
+    Rewrite {
+        searcher: a.clone(),
+        applier: Box::new(move |subst, eg| {
+            if cond(&subst) {
+                let a = pattern_subst(eg, &a, &subst);
+                let b = pattern_subst(eg, &b, &subst);
+                eg.union(&a, &b);
+            }
+        }),
+    }
+}
+
+pub fn mk_rewrite<L: Language + 'static>(eg: &mut EGraph<L>, a: Pattern<L>, b: Pattern<L>) -> Rewrite<L> {
+    mk_rewrite_if(eg, a, b, |_| true)
+}
+
+// Direct rewrites.
+
+pub fn rewrite_if<L: Language>(eg: &mut EGraph<L>, a: Pattern<L>, b: Pattern<L>, cond: impl Fn(&Subst) -> bool) {
+    for subst in ematch_all(eg, &a) {
+        if cond(&subst) {
+            let a = pattern_subst(eg, &a, &subst);
+            let b = pattern_subst(eg, &b, &subst);
+            eg.union(&a, &b);
+        }
+    }
+}
+
+pub fn rewrite<L: Language>(eg: &mut EGraph<L>, a: Pattern<L>, b: Pattern<L>) {
+    rewrite_if(eg, a, b, |_| true);
+}
+
+pub fn rewrite_bi<L: Language>(eg: &mut EGraph<L>, a: Pattern<L>, b: Pattern<L>) {
+    rewrite(eg, a.clone(), b.clone());
+    rewrite(eg, b, a);
+}
+
