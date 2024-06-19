ca69918954a75bba39e3ec0882b72392b6d3dafb
memoryleak47 <memoryleak47@protonmail.ch>
2024-06-19 19:08:43 +0200

    ...

--- 3-miniegg-with-slots/src/i_array/tst.rs
+++ 3-miniegg-with-slots/src/i_array/tst.rs
@@ -25,28 +25,28 @@ fn assert_reaches(start: &str, goal: &str, steps: usize, extra_rules: &[&'static
 }
 
 #[test]
-fn tile_1d() {
+fn array_tile_1d() {
     let start = "(m (* n1 32) f)";
     let goal = "(o j (o (m n1 (m 32 f)) (s 32)))";
     assert_reaches(start, goal, 40, &["transpose-maps", "split-map"]);
 }
 
 #[test]
-fn tile_2d() {
+fn array_tile_2d() {
     let start = "(m (* n1 32) (m (* n2 32) f))";
     let mid = "(o (o (o (m (* n1 32) j) j) (o (m n1 (m 32 (m n2 (m 32 f)))) (m n1 (m 32 (s 32))))) (s 32))";
     let goal = "(o (o (o (o (m (* n1 32) j) j) (m n1 T)) (o (m n1 (m n2 (m 32 (m 32 f)))) (m n1 T))) (o (m n1 (m 32 (s 32))) (s 32)))";
 
-    assert_reaches(start, mid, 40, &["transpose-maps"]);
-    // assert_reaches(mid, goal, 40, &["split-map"]);
+    assert_reaches(start, mid, 40, &["split-map"]);
+    assert_reaches(mid, goal, 40, &["transpose-maps"]);
 }
 
 #[test]
-fn tile_3d() {
+fn array_tile_3d() {
     let start = "(m (* n1 32) (m (* n2 32) (m (* n3 32) f)))";
     let mid = "(o (m (* n1 32) (o (m (* n2 32) j) j)) (o (o j (o (m n1 (m 32 (m n2 (m 32 (m n3 (m 32 f)))))) (s 32))) (m (* n1 32) (o (m n2 (m 32 (s 32))) (s 32)))))";
     let goal = "(o (o (m (* n1 32) (o (m (* n2 32) j) j)) j) (o (o (m n1 (o T (m n2 (o (m 32 T) T)))) (o (m n1 (m n2 (m n3 (m 32 (m 32 (m 32 f)))))) (m n1 (m n2 T)))) (o (o (m n1 (o (m n2 (m 32 T)) T)) (s 32)) (m (* n1 32) (o (m n2 (m 32 (s 32))) (s 32))))))";
 
-    assert_reaches(start, mid, 40, &["transpose-maps"]);
-    // assert_reaches(mid, goal, 40, &["split-map"]);
+    assert_reaches(start, mid, 40, &["split-map"]);
+    assert_reaches(mid, goal, 40, &["transpose-maps"]);
 }
