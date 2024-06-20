777487d1a8d466ccc85f39f8adb1884996e5efc7
memoryleak47 <memoryleak47@protonmail.ch>
2024-06-20 21:13:41 +0200

    take the precisely same normalized inputs as egg-rise

--- 3-miniegg-with-slots/src/main.rs
+++ 3-miniegg-with-slots/src/main.rs
@@ -13,24 +13,19 @@ fn run(name: &str, exp: WithExpansion) {
 
     match name {
         "reduction" => {
-            let start = "(app (lam compose (app (lam add1 (app (app (var compose) (var add1)) (app (app (var compose) (var add1)) (app (app (var compose) (var add1)) (app (app (var compose) (var add1)) (app (app (var compose) (var add1)) (app (app (var compose) (var add1)) (var add1)))))))) (lam y (app (app add (var y)) 1)))) (lam f (lam g (lam x (app (var f) (app (var g) (var x)))))))".into();
-            let goal = "(lam x (app (app add (app (app add (app (app add (app (app add (app (app add (app (app add (app (app add (var x)) 1)) 1)) 1)) 1)) 1)) 1)) 1))".into();
+            let start = "(app (lam compose (app (lam add1 (app (app (var compose) (var add1)) (app (app (var compose) (var add1)) (app (app (var compose) (var add1)) (app (app (var compose) (var add1)) (app (app (var compose) (var add1)) (app (app (var compose) (var add1)) (var add1)))))))) (lam y (app (app add (var y)) 1)))) (lam f (lam g (lam x (app (var f) (app (var g) (var x)))))))";
+            let goal = "(lam x (app (app add (app (app add (app (app add (app (app add (app (app add (app (app add (app (app add (var x)) 1)) 1)) 1)) 1)) 1)) 1)) 1))";
             bench(start, goal, &rules, false)
         },
         "fission" => {
-            let start = "(lam f1 (lam f2 (lam f3 (lam f4 (lam f5 (app map (lam x3 (app (var f5) (app (lam x2 (app (var f4) (app (lam x1 (app (var f3) (app (lam x0 (app (var f2) (app (var f1) (var x0)))) (var x1)))) (var x2)))) (var x3))))))))))".into();
-            let goal = "(lam f1 (lam f2 (lam f3 (lam f4 (lam f5 (lam x7 (app (app map (lam x6 (app (var f5) (app (lam x5 (app (var f4) (app (var f3) (var x5)))) (var x6))))) (app (app map (lam x4 (app (var f2) (app (var f1) (var x4))))) (var x7)))))))))".into();
+            let start = "(lam f1 (lam f2 (lam f3 (lam f4 (lam f5 (app map (lam x11 (app (var f5) (app (var f4) (app (var f3) (app (var f2) (app (var f1) (var x11)))))))))))))";
+            let goal =  "(lam f1 (lam f2 (lam f3 (lam f4 (lam f5 (lam x7 (app (app map (lam x6 (app (var f5) (app (var f4) (app (var f3) (var x6)))))) (app (app map (lam x4 (app (var f2) (app (var f1) (var x4))))) (var x7)))))))))";
             rules.extend(["map-fusion", "map-fission"]);
             bench(start, goal, &rules, true)
         },
         "binomial" => {
-            // DOESN'T WORK:
-            let start =  "(app (app map (app map (lam nbh (app (app (lam a (lam b (app (app (app reduce add) 0) (app (app map (lam mt (app (app mul (app fst (var mt))) (app snd (var mt))))) (app (app zip (var a)) (var b)))))) (app join weights2d)) (app join (var nbh)))))) (app (lam x4 (app (app map transpose) (app (lam x3 (app (app (app slide 3) 1) (app (app map (app (app slide 3) 1)) (var x3)))) (var x4)))) input))".into();
-            let goal = "(app (app map (lam x8 (app (app map (app (lam x0 (lam x1 (app (app (app reduce add) 0) (app (app map (lam x2 (app (app mul (app fst (var x2))) (app snd (var x2))))) (app (app zip (var x0)) (var x1)))))) weightsH)) (app (lam x7 (app (app (app slide 3) 1) (app (lam x6 (app (app map (app (lam a (lam b (app (app (app reduce add) 0) (app (app map (lam mt (app (app mul (app fst (var mt))) (app snd (var mt))))) (app (app zip (var a)) (var b)))))) weightsV)) (app transpose (var x6)))) (var x7)))) (var x8))))) (app (app (app slide 3) 1) input))".into();
-
-            // WORKS:
-            // let start = "(app (app map (app map (lam s0 (app (app (app reduce add) 0) (app (app map (lam s-1 (app (app mul (app fst (var s-1))) (app snd (var s-1))))) (app (app zip (app join weights2d)) (app join (var s0)))))))) (app (app map transpose) (app (app (app slide 3) 1) (app (app map (app (app slide 3) 1)) input))))".into();
-            // let goal = "(app (app map (lam s0 (app (app map (lam s1 (app (app (app reduce add) 0) (app (app map (lam s-2 (app (app mul (app fst (var s-2))) (app snd (var s-2))))) (app (app zip weightsH) (var s1)))))) (app (app (app slide 3) 1) (app (app map (lam s2 (app (app (app reduce add) 0) (app (app map (lam s-3 (app (app mul (app fst (var s-3))) (app snd (var s-3))))) (app (app zip weightsV) (var s2)))))) (app transpose (var s0))))))) (app (app (app slide 3) 1) input))".into();
+            let start = "(lam x17 (app (app map (app map (lam nbh (app (app (app reduce add) 0) (app (app map (lam mt (app (app mul (app fst (var mt))) (app snd (var mt))))) (app (app zip (app join weights2d)) (app join (var nbh)))))))) (app (app map transpose) (app (app (app slide 3) 1) (app (app map (app (app slide 3) 1)) (var x17))))))";
+            let goal = "(lam x26 (app (app map (lam x25 (app (app map (lam x15 (app (app (app reduce add) 0) (app (app map (lam x16 (app (app mul (app fst (var x16))) (app snd (var x16))))) (app (app zip weightsH) (var x15)))))) (app (app (app slide 3) 1) (app (app map (lam b (app (app (app reduce add) 0) (app (app map (lam mt (app (app mul (app fst (var mt))) (app snd (var mt))))) (app (app zip weightsV) (var b)))))) (app transpose (var x25))))))) (app (app (app slide 3) 1) (var x26))))";
 
             rules.extend([
                 "remove-transpose-pair", "map-fusion", "map-fission",
