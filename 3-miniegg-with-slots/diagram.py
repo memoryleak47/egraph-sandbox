da132ec3b7afad322ccecefef80c1c5545f67c0b
memoryleak47 <memoryleak47@protonmail.ch>
2024-06-22 13:13:18 +0200

    ...

--- 3-miniegg-with-slots/diagram.py
+++ 3-miniegg-with-slots/diagram.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3 -B
 
 import matplotlib.pyplot as plt
+import matplotlib
 import numpy as np
+
+plt.rcParams['font.size'] = 22
 
 problems = ("Reduction", "Fission", "Binomial")
 encodings = {
@@ -11,7 +14,7 @@ encodings = {
 }
 
 x = np.arange(len(problems))  # the label locations
-width = 0.25  # the width of the bars
+width = 0.22  # the width of the bars
 multiplier = 0
 
 fig, ax = plt.subplots(layout='constrained')
@@ -23,11 +26,11 @@ for attribute, measurement in encodings.items():
     multiplier += 1
 
 # Add some text for labels, title and custom x-axis tick labels, etc.
-ax.set_ylabel('Length (mm)')
+ax.set_ylabel('Number of e-nodes (log-scale)')
+ax.set_xlabel('Rewrite Problem')
 ax.set_yscale('log')
-ax.set_title('Number of e-nodes by Rewrite Problems')
 ax.set_xticks(x + width, problems)
-# ax.set_ylim(40000)
-ax.legend(loc='upper left', ncols=3)
+ax.set_ylim(0, 40000)
+ax.legend(loc='upper left')
 
 plt.show()
