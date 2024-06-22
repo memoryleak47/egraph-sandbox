d46a6a53248736e17971410d3063bed216e28d29
memoryleak47 <memoryleak47@protonmail.ch>
2024-06-22 17:51:47 +0200

    ...

--- 3-miniegg-with-slots/diagram.py
+++ 3-miniegg-with-slots/diagram.py
@@ -4,14 +4,27 @@ import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
 
+etaexp=True
+
 plt.rcParams['font.size'] = 22
 
 problems = ("Reduction", "Fission", "Binomial")
-encodings = {
-    'Named': (335, 1811085, 1001856),
-    'DeBruijn': (574, 20820, 33177),
-    'Slotted': (299, 184, 19132),
-}
+
+oom = 9999999999999
+
+if etaexp:
+    encodings = {
+        'Named (egg)': (oom, oom, oom),
+        'DeBruijn (egg)': (oom, oom, oom),
+        'Slotted': (4304, 1334, 96523),
+    }
+else:
+    encodings = {
+        'Named (egg)': (335, oom, oom),
+        'DeBruijn (egg)': (574, 20820, 33177),
+        'Slotted': (299, 184, 19132),
+    }
+
 
 x = np.arange(len(problems))  # the label locations
 width = 0.22  # the width of the bars
@@ -30,7 +43,11 @@ ax.set_ylabel('Number of e-nodes (log-scale)')
 ax.set_xlabel('Rewrite Problem')
 ax.set_yscale('log')
 ax.set_xticks(x + width, problems)
-ax.set_ylim(0, 100000)
-ax.legend(loc='upper left')
+if etaexp:
+    ax.set_ylim(0, 200000)
+    ax.legend(loc='lower left')
+else:
+    ax.set_ylim(0, 100000-1)
+    ax.legend(loc='upper left')
 
 plt.show()
