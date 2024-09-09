a91543165f4ecefd7d2c0cafa8ee22d9ccf320ba
memoryleak47 <memoryleak47@protonmail.ch>
2024-09-09 15:53:39 +0200

    fix alpha_eq

--- 3-miniegg-with-slots/src/egraph/explain.rs
+++ 3-miniegg-with-slots/src/egraph/explain.rs
@@ -467,34 +467,47 @@ fn get_applied(map: &HashMap<Id, AppliedId>, k: &AppliedId) -> Option<AppliedId>
 
 
 fn alpha_eq<L: Language>(a: &RecExpr<L>, b: &RecExpr<L>) -> bool {
-    alpha_eq_impl(a, b, &mut Default::default())
+    alpha_eq_impl(a, b, Default::default())
 }
 
 // we assume that all slots come up either free, or bound but not both inside of a single term.
-// `map` maps the slotnames from a to b.
-fn alpha_eq_impl<L: Language>(a: &RecExpr<L>, b: &RecExpr<L>, map: &mut SlotMap) -> bool {
+// `map` maps the *bound* slot names from a to b.
+fn alpha_eq_impl<L: Language>(a: &RecExpr<L>, b: &RecExpr<L>, map: SlotMap) -> bool {
+    let mut map = map;
+
+    // weak shape check.
     if a.node.weak_shape().0 != b.node.weak_shape().0 {
         return false;
     }
 
+    // private slot introduction.
+    let sa = a.node.private_slot_occurences().into_iter();
+    let sb = b.node.private_slot_occurences().into_iter();
+
+    for (x, y) in sa.zip(sb) {
+        if map.keys().contains(&x) { return false; }
+        if map.values().contains(&y) { return false; }
+        map.insert(x, y);
+    }
+
+    // general slot check.
     let sa = a.node.all_slot_occurences().into_iter();
     let sb = b.node.all_slot_occurences().into_iter();
 
     for (x, y) in sa.zip(sb) {
-        if !safe_insert(x, y, map) { return false; }
+        if map.keys().contains(&x) || map.values().contains(&y) {
+            // check bound slot equality.
+            if map.get(x) != Some(y) { return false; }
+        } else {
+            // check unbound slot equality.
+            if x != y { return false; }
+        }
     }
 
+    // recursion check.
     for (l, r) in a.children.iter().zip(b.children.iter()) {
-        if !alpha_eq_impl(l, r, map) { return false; }
+        if !alpha_eq_impl(l, r, map.clone()) { return false; }
     }
 
-    true
-}
-
-fn safe_insert(x: Slot, y: Slot, map: &mut SlotMap) -> bool {
-    if map.get(x) == Some(y) { return true; }
-    if map.keys().contains(&x) { return false; }
-    if map.values().contains(&y) { return false; }
-    map.insert(x, y);
     true
 }
