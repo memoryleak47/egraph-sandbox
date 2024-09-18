8900a21b976a37a1fef906dd4fc5cfc5c8cbfd41
memoryleak47 <memoryleak47@protonmail.ch>
2024-09-18 09:10:01 +0200

    ...

--- 3-miniegg-with-slots/src/egraph/expl/mod.rs
+++ 3-miniegg-with-slots/src/egraph/expl/mod.rs
@@ -7,13 +7,10 @@ impl<L: Language> EGraph<L> {
     pub fn explain_equivalence(&mut self, t1: RecExpr<L>, t2: RecExpr<L>) -> ProvenEq {
         let i1 = self.add_synt_expr(t1);
         let i2 = self.add_synt_expr(t2);
-        let p1 = self.explain_lookup(&i1);
-        let p2 = self.explain_lookup(&i2);
-        todo!() // chain p1 with p2 and return.
-    }
-
-    // proofs that synt_term(i) and synt_term(lookup(i)) are semantically equal.
-    fn explain_lookup(&self, i: &AppliedId) -> ProvenEq {
-        todo!()
+        todo!() // TODO
+        // let p1 = self.unionfind[i1].pointer_to_leader;
+        // let p2 = self.unionfind[i2].pointer_to_leader;
+        // let (p1, p2) = self.group_permute_to_match(p1, p2);
+        // return p1.chain(p2);
     }
 }
