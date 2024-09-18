6c5210f8f38795ed9a32919dcda82adfad8a34c5
memoryleak47 <memoryleak47@protonmail.ch>
2024-09-18 18:11:39 +0200

    finish basic version of explain_equivalence

--- 3-miniegg-with-slots/src/egraph/expl/mod.rs
+++ 3-miniegg-with-slots/src/egraph/expl/mod.rs
@@ -7,13 +7,18 @@ mod step;
 pub use step::*;
 
 impl<L: Language> EGraph<L> {
-    pub fn explain_equivalence(&mut self, t1: RecExpr<L>, t2: RecExpr<L>) -> ProvenEq {
+    pub fn explain_equivalence(&mut self, t1: RecExpr<L>, t2: RecExpr<L>) -> Arc<ProvenEq> {
         let i1 = self.add_syn_expr(t1);
         let i2 = self.add_syn_expr(t2);
-        todo!() // TODO
-        // let p1 = self.unionfind[i1].pointer_to_leader;
-        // let p2 = self.unionfind[i2].pointer_to_leader;
-        // let (p1, p2) = self.group_permute_to_match(p1, p2);
-        // return p1.chain(p2);
+
+        if !self.eq(&i1, &i2) { panic!("Can't explain an equivalence that does not hold!"); }
+
+        let (norm1, prf1) = self.unionfind.get_proof(i1.id, self);
+        let (norm2, prf2) = self.unionfind.get_proof(i2.id, self);
+        let prf2 = self.prove_symmetry(prf2);
+        let p = self.prove_transitivity(prf1, prf2);
+        assert_eq!(&p.eq().l, &i1);
+        assert_eq!(&p.eq().r, &i2);
+        p
     }
 }
