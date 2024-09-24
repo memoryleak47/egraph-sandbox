c2209004eba6a107f15346dc225553d631898a84
memoryleak47 <memoryleak47@protonmail.ch>
2024-09-24 13:36:46 +0200

    ...

--- 3-miniegg-with-slots/src/egraph/expl/step.rs
+++ 3-miniegg-with-slots/src/egraph/expl/step.rs
@@ -1,20 +1,24 @@
 use crate::*;
 
+#[track_caller]
 pub fn prove_explicit(l: &AppliedId, r: &AppliedId, j: Option<String>) -> ProvenEq {
     let eq = Equation { l: l.clone(), r: r.clone() };
     ExplicitProof(j).check(&eq).unwrap()
 }
 
+#[track_caller]
 pub fn prove_reflexivity(id: &AppliedId) -> ProvenEq {
     let eq = Equation { l: id.clone(), r: id.clone() };
     ReflexivityProof.check(&eq).unwrap()
 }
 
+#[track_caller]
 pub fn prove_symmetry(x: ProvenEq) -> ProvenEq {
     let eq = Equation { l: x.r.clone(), r: x.l.clone() };
     SymmetryProof(x).check(&eq).unwrap()
 }
 
+#[track_caller]
 pub fn prove_transitivity(x: ProvenEq, y: ProvenEq) -> ProvenEq {
     let eq1 = x.clone();
     let eq2 = y.clone();
@@ -26,6 +30,7 @@ pub fn prove_transitivity(x: ProvenEq, y: ProvenEq) -> ProvenEq {
     TransitivityProof(x.clone(), y.clone()).check(&eq).unwrap()
 }
 
+#[track_caller]
 pub fn prove_congruence<L: Language>(l: &AppliedId, r: &AppliedId, child_proofs: Vec<ProvenEq>, eg: &EGraph<L>) -> ProvenEq {
     let eq = Equation { l: l.clone(), r: r.clone() };
     CongruenceProof(child_proofs).check(&eq, eg).unwrap()
