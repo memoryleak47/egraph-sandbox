59cdf0a8b9b4f1123a34e2d691c6b81a1fa8e0bf
memoryleak47 <memoryleak47@protonmail.ch>
2024-10-01 13:22:48 +0200

    add more checks to main tests

--- 3-miniegg-with-slots/src/main.rs
+++ 3-miniegg-with-slots/src/main.rs
@@ -148,24 +148,32 @@ fn main2() {
 // misc functions.
 
 fn id<L: Language>(s: &str, eg: &mut EGraph<L>) -> AppliedId {
+    eg.check();
     let re = RecExpr::parse(s).unwrap();
-    eg.add_syn_expr(re.clone())
+    let out = eg.add_syn_expr(re.clone());
+    eg.check();
+    out
 }
 
 fn term<L: Language>(s: &str, eg: &mut EGraph<L>) -> RecExpr<L> {
     let re = RecExpr::parse(s).unwrap();
-    eg.add_syn_expr(re.clone());
     re
 }
 
 fn equate<L: Language>(s1: &str, s2: &str, eg: &mut EGraph<L>) {
+    eg.check();
     let s1 = id(s1, eg);
+    eg.check();
     let s2 = id(s2, eg);
+    eg.check();
     eg.union(&s1, &s2);
+    eg.check();
 }
 
 fn explain<L: Language>(s1: &str, s2: &str, eg: &mut EGraph<L>) {
+    eg.check();
     let s1 = term(s1, eg);
     let s2 = term(s2, eg);
     eg.explain_equivalence(s1, s2).show_expr(eg);
+    eg.check();
 }
