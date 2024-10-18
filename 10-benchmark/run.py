b8008faeff5af7e64bc78f9dd89dce07e5f6a84b
memoryleak47 <memoryleak47@protonmail.ch>
2024-10-18 19:12:56 +0200

    ...

--- 10-benchmark/run.py
+++ 10-benchmark/run.py
@@ -3,13 +3,21 @@
 import os
 from gen import generate
 
+os.system("mkdir -p outputs")
+os.system("rm outputs/*")
 os.system("cd egg-rise; cargo b --release")
 os.system("cd slotted-rise; cargo b --release")
 
-for N in range(1, 20):
-    for M in range(1, 20):
+for N in range(1, 8):
+    for M in range(1, 8):
+        print(N, M, "egg-name")
         lhs, rhs = generate(N, M, "egg")
-        os.system(f"./egg-rise/target/release/egg-rise '{lhs}' '{rhs}' name")
+        os.system(f"./egg-rise/target/release/egg-rise '{lhs}' '{rhs}' name &> ./outputs/egg-name-{N}-{M}")
 
+        print(N, M, "egg-db")
+        lhs, rhs = generate(N, M, "egg")
+        os.system(f"./egg-rise/target/release/egg-rise '{lhs}' '{rhs}' de-bruijn &> ./outputs/egg-db-{N}-{M}")
+
+        print(N, M, "slotted")
         lhs, rhs = generate(N, M, "slotted")
-        os.system(f"./slotted-rise/target/release/slotted-rise '{lhs}' '{rhs}'")
+        os.system(f"./slotted-rise/target/release/slotted-rise '{lhs}' '{rhs}' &> ./outputs/slotted-{N}-{M}")
