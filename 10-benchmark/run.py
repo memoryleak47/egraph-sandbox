995643d8a8336bfad68b7812d3081476cd22536a
memoryleak47 <memoryleak47@protonmail.ch>
2024-10-18 19:52:53 +0200

    ...

--- 10-benchmark/run.py
+++ 10-benchmark/run.py
@@ -8,16 +8,17 @@ os.system("rm outputs/*")
 os.system("cd egg-rise; cargo b --release")
 os.system("cd slotted-rise; cargo b --release")
 
-for N in range(1, 8):
-    for M in range(1, 8):
+for N in range(1, 21):
+    for M in range(1, 21):
         print(N, M, "egg-name")
         lhs, rhs = generate(N, M, "egg")
-        os.system(f"/usr/bin/time -f '%E, %M Kbytes' timeout -v 5m ./egg-rise/target/release/egg-rise '{lhs}' '{rhs}' name &> ./outputs/egg-name-{N}-{M}")
+        if N == 1 or M == 1 or (os.path.exists(f"./outputs/egg-name-{N-1}-{M}") and os.path.exists(f"./outputs/egg-name-{N}-{M-1}")):
+            os.system(f"/usr/bin/time -f '%E, %M Kbytes' timeout -v 20m ./egg-rise/target/release/egg-rise '{lhs}' '{rhs}' name &> ./outputs/egg-name-{N}-{M}")
 
         print(N, M, "egg-db")
         lhs, rhs = generate(N, M, "egg")
-        os.system(f"/usr/bin/time -f '%E, %M Kbytes' timeout -v 5m ./egg-rise/target/release/egg-rise '{lhs}' '{rhs}' de-bruijn &> ./outputs/egg-db-{N}-{M}")
+        os.system(f"/usr/bin/time -f '%E, %M Kbytes' timeout -v 20m ./egg-rise/target/release/egg-rise '{lhs}' '{rhs}' de-bruijn &> ./outputs/egg-db-{N}-{M}")
 
         print(N, M, "slotted")
         lhs, rhs = generate(N, M, "slotted")
-        os.system(f"/usr/bin/time -f '%E, %M Kbytes' timeout -v 5m ./slotted-rise/target/release/slotted-rise '{lhs}' '{rhs}' &> ./outputs/slotted-{N}-{M}")
+        os.system(f"/usr/bin/time -f '%E, %M Kbytes' timeout -v 20m ./slotted-rise/target/release/slotted-rise '{lhs}' '{rhs}' &> ./outputs/slotted-{N}-{M}")
