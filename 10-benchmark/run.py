ca4d99daa5f1cc5398263c10428e25a51c4a5158
memoryleak47 <memoryleak47@protonmail.ch>
2024-10-18 19:06:01 +0200

    ...

--- 10-benchmark/run.py
+++ 10-benchmark/run.py
@@ -3,10 +3,13 @@
 import os
 from gen import generate
 
-N, M = 3, 3
+os.system("cd egg-rise; cargo b --release")
+os.system("cd slotted-rise; cargo b --release")
 
-lhs, rhs = generate(N, M, "egg")
-os.system(f"cd egg-rise; cargo r --release '{lhs}' '{rhs}' name")
+for N in range(1, 20):
+    for M in range(1, 20):
+        lhs, rhs = generate(N, M, "egg")
+        os.system(f"./egg-rise/target/release/egg-rise '{lhs}' '{rhs}' name")
 
-lhs, rhs = generate(N, M, "slotted")
-os.system(f"cd slotted-rise; cargo r --release '{lhs}' '{rhs}'")
+        lhs, rhs = generate(N, M, "slotted")
+        os.system(f"./slotted-rise/target/release/slotted-rise '{lhs}' '{rhs}'")
