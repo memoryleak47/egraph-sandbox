ef96164da2c7c44f2e0ff846feb167c4ffcad18c
memoryleak47 <memoryleak47@protonmail.ch>
2024-10-21 17:40:07 +0200

    ...

--- 10-benchmark/run.py
+++ 10-benchmark/run.py
@@ -3,6 +3,7 @@
 import os
 from gen import generate
 
+os.system("rm -r outputs")
 os.system("mkdir -p outputs")
 os.system("cd egg-rise; cargo b --release")
 os.system("cd slotted-rise; cargo b --release")
@@ -33,11 +34,8 @@ for (N, M) in GRID:
     lhs, rhs = generate(N, M, VARS)
     var = "var" if VARS else "novar"
 
-    # print(N, M, "egg-name")
-    # os.system(f"/usr/bin/time -f '%E, %M Kbytes' timeout -v 20m ./egg-rise/target/release/egg-rise '{lhs}' '{rhs}' name &> ./outputs/egg-name-{N}-{M}")
-
     print(N, M, "egg-db")
-    os.system(f"/usr/bin/time -f '%E, %M Kbytes' timeout -v 20m ./egg-rise/target/release/egg-rise '{lhs}' '{rhs}' de-bruijn &> ./outputs/egg-db-{N}-{M}-{var}")
+    os.system(f"/usr/bin/time -f '%E, %M Kbytes' timeout -v 20m ./egg-rise/target/release/egg-rise '{lhs}' '{rhs}' de-bruijn ./outputs/egg-db-{N}-{M}-{var}.csv &> /dev/null")
 
     print(N, M, "slotted")
-    os.system(f"/usr/bin/time -f '%E, %M Kbytes' timeout -v 20m ./slotted-rise/target/release/slotted-rise '{lhs}' '{rhs}' &> ./outputs/slotted-{N}-{M}-{var}")
+    os.system(f"/usr/bin/time -f '%E, %M Kbytes' timeout -v 20m ./slotted-rise/target/release/slotted-rise '{lhs}' '{rhs}' ./outputs/slotted-{N}-{M}-{var}.csv &> /dev/null")
