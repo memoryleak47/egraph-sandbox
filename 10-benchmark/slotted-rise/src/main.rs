9ec7a57086e20815cc598c9e366260a26cab4663
Thomas Koehler <thomas.koehler@thok.eu>
2024-10-28 22:09:21 +0100

    tracing

--- 10-benchmark/slotted-rise/src/main.rs
+++ 10-benchmark/slotted-rise/src/main.rs
@@ -17,7 +17,10 @@ pub use std::ops::RangeInclusive;
 use memory_stats::memory_stats;
 use std::time::Instant;
 
+use tracing::*;
+
 fn assert_reaches<W>(start: &str, goal: &str, mut csv_out: W, steps: usize) where W: std::io::Write {
+    let init_span = trace_span!("init").entered();
     let start = RecExpr::parse(start).unwrap();
     let goal = RecExpr::parse(goal).unwrap();
 
@@ -25,10 +28,12 @@ fn assert_reaches<W>(start: &str, goal: &str, mut csv_out: W, steps: usize) wher
 
     let mut eg = EGraph::new();
     let i1 = eg.add_expr(start.clone());
+    init_span.exit();
     for iteration in 0..steps {
         dbg!(eg.total_number_of_nodes());
         let start_time = Instant::now();
         apply_rewrites(&mut eg, &rules);
+        let check_span = trace_span!("check").entered();
         if let Some(i2) = lookup_rec_expr(&goal, &eg) {
             if eg.eq(&i1, &i2) {
                 dbg!(eg.total_number_of_nodes());
@@ -38,6 +43,7 @@ fn assert_reaches<W>(start: &str, goal: &str, mut csv_out: W, steps: usize) wher
                 return;
             }
         }
+        check_span.exit();
         let out_of_memory = iteration_stats(&mut csv_out, iteration, &eg, false, start_time);
         if out_of_memory {
             dbg!("reached memory limit!");
@@ -45,7 +51,7 @@ fn assert_reaches<W>(start: &str, goal: &str, mut csv_out: W, steps: usize) wher
         }
     }
 
-    dbg!(extract::<_, _, AstSizeNoLet>(i1, &eg));
+    dbg!(extract::<_, _, AstSizeNoLet>(&i1, &eg));
     dbg!(&goal);
     assert!(false);
 }
@@ -59,6 +65,7 @@ fn assert_reaches<W>(start: &str, goal: &str, mut csv_out: W, steps: usize) wher
 // e-graph classes,
 // total time,
 // found
+#[tracing::instrument(level = "trace", skip_all)]
 fn iteration_stats<W, L, N>(csv_out: &mut W, it_number: usize, eg: &EGraph<L, N>, found: bool, start_time: Instant) -> bool
     where W: std::io::Write, L: Language, N: Analysis<L>
 {
@@ -83,5 +90,97 @@ fn main() {
     let rhs = &args[1];
     let csv_out = &args[2];
     let mut csv_f = std::fs::File::create(csv_out).unwrap();
-    assert_reaches(lhs, rhs, csv_f, 60);
+
+    if cfg!(feature = "tracing") {
+        use tracing_subscriber;
+        use tracing_subscriber::layer::SubscriberExt;
+        use tracing_subscriber::prelude::*;
+        use tracing_profile::*;
+
+        println!("<TRACING>");
+        /*
+        tracing_subscriber::fmt()
+            .with_span_events(tracing_subscriber::fmt::format::FmtSpan::FULL)
+            .with_max_level(tracing_subscriber::filter::LevelFilter::TRACE)
+            .init();
+            */
+
+        tracing_subscriber::registry()
+            .with(PrintTreeLayer::default())
+            // .with(CsvLayer::new("/tmp/slotted-rise-tracing.csv"))
+            .init();
+
+        let span = trace_span!("root");
+        span.in_scope(|| {
+            assert_reaches(lhs, rhs, csv_f, 60);
+        });
+/*
+        DEPRECATED (tracing-timing):
+
+        use tracing::{dispatcher, Dispatch};
+        use tracing_timing::{Builder, Histogram};
+
+        let subscriber = Builder::default().build(|| Histogram::new_with_max(1_000_000, 2).unwrap());
+        let sid = subscriber.downcaster();
+        let dispatcher = Dispatch::new(subscriber);
+        dispatcher::with_default(&dispatcher, || {
+            assert_reaches(lhs, rhs, csv_f, 60);
+        });
+
+        sid.downcast(&dispatcher).unwrap().with_histograms(|hs| {
+            for (span_group, hs) in hs {
+                for (event_group, h) in hs {
+                    println!("span: {}, event: {}", span_group, event_group);
+
+                    println!(
+                        "mean: {:.1}µs, p50: {}µs, p90: {}µs, p99: {}µs, p999: {}µs, max: {}µs",
+                        h.mean() / 1000.0,
+                        h.value_at_quantile(0.5) / 1_000,
+                        h.value_at_quantile(0.9) / 1_000,
+                        h.value_at_quantile(0.99) / 1_000,
+                        h.value_at_quantile(0.999) / 1_000,
+                        h.max() / 1_000,
+                    );
+                    for v in break_once(
+                        h.iter_linear(25_000).skip_while(|v| v.quantile() < 0.01),
+                        |v| v.quantile() > 0.95,
+                    ) {
+                        println!(
+                            "{:4}µs | {:40} | {:4.1}th %-ile",
+                            (v.value_iterated_to() + 1) / 1_000,
+                            "*".repeat(
+                                (v.count_since_last_iteration() as f64 * 40.0 / h.len() as f64).ceil() as usize
+                            ),
+                            v.percentile(),
+                        );
+                    }
+                }
+            }
+        });
+        */
+    } else {
+        assert_reaches(lhs, rhs, csv_f, 60);
+    }
+}
+
+#[cfg(feature = "tracing")]
+fn break_once<I, F>(it: I, mut f: F) -> impl Iterator<Item = I::Item>
+where
+    I: IntoIterator,
+    F: FnMut(&I::Item) -> bool,
+{
+    let mut got_true = false;
+    it.into_iter().take_while(move |i| {
+        if got_true {
+            // we've already yielded when f was true
+            return false;
+        }
+        if f(i) {
+            // this must be the first time f returns true
+            // we should yield i, and then no more
+            got_true = true;
+        }
+        // f returned false, so we should keep yielding
+        true
+    })
 }
